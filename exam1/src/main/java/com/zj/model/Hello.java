f2d28e711dfd1853d5ff1878dd127ccc5eaf887f
zhang <1152765051@qq.com>
2019-03-16 09:17:18 +0800

    项目初始化

---
+++ exam1/src/main/java/com/zj/model/Hello.java
@@ -0,0 +1,11 @@
+package com.zj.model;
+
+public class Hello {
+
+	public void show() {
+		
+		System.out.println("HelloWorld!!");
+		
+	}
+	
+}
---
+++ exam1/src/test/java/com/zj/test/TestHello.java
@@ -0,0 +1,19 @@
+package com.zj.test;
+
+
+import org.junit.Test;
+
+import com.zj.model.Hello;
+
+public class TestHello {
+
+	@Test
+	public void test() {
+		
+		Hello h = new Hello();
+		
+		h.show();
+		
+	}
+
+}
